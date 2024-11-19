2545f11d7364457d56d32e641b0bfee7cbdb6254
lk <long.nguyen@cubicasa.com>
2024-11-19 09:22:45 +0700

    Down to 2

--- validator.mjs
+++ validator.mjs
@@ -1,11 +1,11 @@
-import blueprint from "./plutus.json" assert { type: "json" };
+import blueprint from "./plutus.json" with { type: "json" };
 
 export function getValidators() {
-    const mintMintingPolicy = blueprint.validators.find(v => v.title === "mint.token.mint");
+    const mintMintingPolicy = blueprint.validators.find(v => v.title === "mint.mint");
     if (!mintMintingPolicy) {
         throw new Error('mintMintingPolicy not found');
     }
-    
+
     return {
         mintMintingPolicy: {
             type: "PlutusV2",
